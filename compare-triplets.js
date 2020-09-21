0c18e8f1775f39b8ee49c29891c6d14d98758f67
bret-gibson <bret.gibson@gmail.com>
2020-09-20 23:57:56 -0400

    added solution

---
+++ compare-triplets.js
@@ -0,0 +1,24 @@
+const compareTriplets = (a, b) => {
+  let alice = 0;
+  let bob = 0;
+  let finalArr = [];
+
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] > b[i]) {
+      alice += 1;
+    } else {
+      bob += 1;
+    }
+  }
+  finalArr.push(alice);
+  finalArr.push(bob);
+  console.log(finalArr);
+  return finalArr;
+};
+
+let alice = [17, 28, 30];
+let bob = [99, 16, 8];
+
+compareTriplets(alice, bob);
+
+//outputs 2 1 which is correct
